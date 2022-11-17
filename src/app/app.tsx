2c6d01a5209c29bf8d767fade6fd856320d6e251
Iván Sánchez <sanchez.diaz.ivan@gmail.com>
2022-11-17 21:24:08 +0100

    🎉 Started project

---
+++ src/app/app.tsx
@@ -0,0 +1,26 @@
+import { useState } from 'preact/hooks';
+import './app.css'
+
+export function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+        <main>
+            <header>
+
+            </header>
+            <section>
+                <article></article>
+            </section>
+            <nav>
+
+            </nav>
+            <footer>
+
+            </footer>
+        </main>
+
+    </>
+  );
+}
